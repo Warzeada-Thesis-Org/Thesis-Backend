2362379b6e50e4ecfd8e7efb00e5bed1758644c0
Adam Warzel <warzeada@fit.cvut.cz>
2023-01-02 23:10:52 +0100

    Initialize Gradle project

---
+++ src/test/java/cz/cvut/fit/warzeada/thesis/ThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.warzeada.thesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
